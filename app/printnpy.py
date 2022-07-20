1dd55bb3e1e9a3cfd123a9913e8a32395baa87db
Juan.py <jp.crespo.vargas@gmail.com>
2022-07-20 06:27:52 -0400

    auto post

--- app/printnpy.py
+++ app/printnpy.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 try:
-	a = np.load('respuestas.npy',allow_pickle='TRUE').tolist()
+	#a = np.load('respuestas.npy',allow_pickle='TRUE').tolist()
+	a = np.load('estados.npy',allow_pickle='TRUE').tolist()
 	if(a):
 		print(a)
 	else: print('log vacío')
